ca60c869cf8b017af6bf514e7d3d60300617ea57
Augusto Costa <criartsoftech@outlook.com>
2024-11-17 23:45:38 -0300

    testcommit00100

--- src/services/license/LicenseCheck.service.ts
+++ src/services/license/LicenseCheck.service.ts
@@ -3,28 +3,28 @@ import prismaClient from "../../prisma";
 import { LicenseCheckReq } from "../../models/license/LicenseModels";
 
 class LicenseCheckService {
-  async execute({ fkPer, nrle }: LicenseCheckReq) {
-    const check = await prismaClient.license.findFirst({
-      where: {
-        fkPer: fkPer,
-        nrle: nrle,
-      },
-      select: {
-        pkLic: true,
-        university: true,
-        nrle: true,
-        nisr: true,
-        period: {
-          select: {
-            pkPer: true,
-            year: true,
-          }
-        }
-      },
-    });
+    async execute({ fkPer, nrle }: LicenseCheckReq) {
+        const check = await prismaClient.license.findFirst({
+            where: {
+                fkPer: fkPer,
+                nrle: nrle,
+            },
+            select: {
+                pkLic: true,
+                university: true,
+                nrle: true,
+                nisr: true,
+                period: {
+                    select: {
+                        pkPer: true,
+                        year: true,
+                    }
+                }
+            },
+        });
 
-    return check;
-  }
+        return check;
+    }
 }
 
 export { LicenseCheckService };
